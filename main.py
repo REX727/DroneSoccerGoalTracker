c9c31a0102e4d285f4f793a7d64bb3482a9cc572
Rex Kwong <rexkwong727@gmail.com>
2026-01-20 23:58:21 +0800

    V1.0.1 Added New Functions

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import CLI_Style as s
+import datetime
 
-print(f"{s.BOLD}Drone Soccer Goal Tracker{s.END} - V1.0.0")
+print(f"{s.BOLD}Drone Soccer Goal Tracker{s.END} - V1.1.0")
 
 lap_data = []
 
-ProfileName = input("Enter the name:")
+UserName = input(f"Enter the {s.BOLD}User Name{s.END}: ")
+ProfileName = input(f"Enter the {s.BOLD}Profile Name{s.END}: ") 
+Date = datetime.date.today()
+DateString = Date.strftime("%Y%m%d")
+path = DateString + "_" + UserName + "_" + ProfileName
+
 UserInput = ""
 
 print(f"Enter lap time in seconds or type '{s.RBOLD}exit{s.END}' to quit:")
@@ -21,8 +27,8 @@ while UserInput.lower() != "exit":
         AvgTimes = round((sum(lap_data))/len(lap_data),2)
         
         print(f"{s.RBOLD}Exiting Program{s.END}")
-        print(s.BOLD + "\n=== Summary ===" + s.END)
-        print(f"Profile Name: {s.BOLD}{ProfileName}{s.END}\n")
+        print(s.BOLD + "\n====="+path+"=====" + s.END)
+        print(f"Profile Name: {s.BOLD}{ProfileName}{s.END}")
         print(f"Total Goals: {s.GBOLD}{len(lap_data)}{s.END}")
         print(f"Average Seconds Per Goal: {s.GBOLD}{AvgTimes}s{s.END}(rounded to 2 decimal places))")
         break

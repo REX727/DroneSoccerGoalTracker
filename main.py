35cc80650c776891363c318d95668fbe31fc3f94
Rex Kwong <rexkwong727@gmail.com>
2026-01-21 17:15:28 +0800

    V1.2.0 - Fix Bugs & Added New Functions

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 import CLI_Style as s
+import ui 
 import datetime
 
-print(f"{s.BOLD}Drone Soccer Goal Tracker{s.END} - V1.1.0")
+print(f"{s.BOLD}Drone Soccer Goal Tracker{s.END} - V1.2.0")
 
 lap_data = []
 
-UserName = input(f"Enter the {s.BOLD}User Name{s.END}: ")
-ProfileName = input(f"Enter the {s.BOLD}Profile Name{s.END}: ") 
-Date = datetime.date.today()
-DateString = Date.strftime("%Y%m%d")
-path = DateString + "_" + UserName + "_" + ProfileName
 
-UserInput = ""
+path = ui.UserInfo()
+lap_data = ui.UserPrompt(path)
 
-print(f"Enter lap time in seconds or type '{s.RBOLD}exit{s.END}' to quit:")
-
-while UserInput.lower() != "exit":
-    UserInput = input(f"{s.GBOLD}GOAL {len(lap_data)+1}{s.END}: ")
-    if UserInput.lower() != "exit":
-            try:
-                lap_data.append(float(UserInput))
-            except:
-                print(f"{s.RBOLD}VALUE ERROR{s.END}")
-                pass
-    if UserInput.lower() == "exit":
-        AvgTimes = round((sum(lap_data))/len(lap_data),2)
-        
-        print(f"{s.RBOLD}Exiting Program{s.END}")
-        print(s.BOLD + "\n====="+path+"=====" + s.END)
-        print(f"Profile Name: {s.BOLD}{ProfileName}{s.END}")
-        print(f"Total Goals: {s.GBOLD}{len(lap_data)}{s.END}")
-        print(f"Average Seconds Per Goal: {s.GBOLD}{AvgTimes}s{s.END}(rounded to 2 decimal places))")
-        break
+ui.ExitProgram(path, lap_data)

b9ec63f4db8775da542553d70f63cef7ca0c19e2
Rex Kwong <rexkwong727@gmail.com>
2026-01-21 17:17:11 +0800

    V1.2.1 - Fixed Bugs

--- CLI_Style.py
+++ CLI_Style.py
@@ -3,3 +3,5 @@ END = '\033[0m'
 GBOLD = BOLD + '\033[32m'
 RBOLD = BOLD + '\033[31m'
 Split = "===================================================================================="
+version = "1.2.1"
+welcome_msg = print(f"{BOLD}Drone Soccer Goal Tracker{END} - V{version}")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import CLI_Style as s
 import ui 
 import datetime
 
-print(f"{s.BOLD}Drone Soccer Goal Tracker{s.END} - V1.2.0")
+
 
 lap_data = []
 

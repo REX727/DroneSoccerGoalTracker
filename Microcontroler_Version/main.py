f3b39ae96c4adef4546eb3db23c2f23d66571c71
Rex Kwong <rexkwong727@gmail.com>
2026-01-23 19:38:31 +0800

    V1.5.1a - Fixed Bugs

---
+++ Microcontroler_Version/main.py
@@ -0,0 +1,42 @@
+from machine import Pin, Timer
+import TM1637
+
+import time
+import os
+
+tm = TM1637(clk=Pin(27), dio=Pin(26))
+
+tm.write([0, 0, 0, 0])
+
+buzzer = Pin(21, Pin.OUT)
+
+goal_data = [4,3,2,4,5,3]
+
+for i in range(5):
+    buzzer.on()
+    time.sleep_ms(50)
+    buzzer.off()
+    time.sleep_ms(25)
+    buzzer.on()
+    time.sleep_ms(50)
+    buzzer.off()
+    tm.show(str(5-i))
+    time.sleep(1)
+    
+tm.show("----")
+time.sleep_ms(50)
+tm.write([0, 0, 0, 0])
+time.sleep_ms(50)
+tm.show("----")
+time.sleep_ms(50)
+tm.write([0, 0, 0, 0])
+
+for lap in range(len(goal_data)):
+    buzzer.on()
+    time.sleep_ms(50)
+    buzzer.off()
+    tm.show(str(lap+1))
+    time.sleep(goal_data[lap])
+
+
+tm.write([0, 0, 0, 0])
